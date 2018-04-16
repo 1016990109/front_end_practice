d6df47401683db4d4ca517738fbbd78bb0ddaa63
洪传旺 <mf1732047@smail.nju.edu.cn>
2018-04-16 09:05:35 +0800

    express temp

--- src/node/express.js
+++ src/node/express.js
@@ -1,17 +1,49 @@
- let express = require('express')
+let express = require('express')
+let cookieParser = require('cookie-parser')
+let bodyParser = require('body-parser')
+let multiparty = require('multiparty')
+let util = require('util')
+let fs = require('fs')
+let app = express()
 
- let app = express()
+//静态资源
+app.use(express.static('public'))
 
- app.get('/', (req, res) => {
-   res.jsonp()
-   res.send('Hello World!')
+//解析内容
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
-   console.log("请求协议：%s", req.protocol)
- })
+//使用cookie
+app.use(cookieParser())
 
- let server = app.listen(8081, () => {
-   let host = server.address().address
-   let port = server.address().port
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+  console.log("Cookies: " + util.inspect(req.cookies));
 
-   console.log("应用实例，访问地址为 http://%s:%s", host, port)
- })
+  console.log("请求协议：%s", req.protocol)
+})
+
+app.get('/userList', (req, res) => {
+  fs.readFile(__dirname + '/' + 'users.json', 'utf8', (err, data) => {
+    console.log(data)
+    res.end(data)
+  })
+})
+
+app.post('/addUser', (req, res) => {
+  fs.readFile(__dirname + '/' + 'users.json', 'utf8', (err, data) => {
+    let users = JSON.parse(data)
+    let form = new multiparty.Form()
+    let newUser = req.body
+    form.parse(req, (err, fields, files) => {
+      fields
+    })
+  })
+})
+
+let server = app.listen(8081, () => {
+  let host = server.address().address
+  let port = server.address().port
+
+  console.log("应用实例，访问地址为 http://%s:%s", host, port)
+})
