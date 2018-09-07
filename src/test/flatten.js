2908fc03e3277f666ee23bbf8c92a5551117419d
hongchuanwang <Hcw960810>
2018-09-07 10:09:06 +0800

    [add] flatten.js

---
+++ src/test/flatten.js
@@ -0,0 +1,40 @@
+const input = [{
+  a: 1,
+  b: [1, 2, { c: true }, [3]],
+  d: { e: 2, f: 3 },
+  g: null
+}]
+
+function flatten(arr) {
+  let result = {}
+
+  function deep(arr, key, isObject) {
+    if (typeof arr === 'object') {
+      for (let i in arr) {
+        if (typeof arr[i] === 'object') {
+          if (Array.isArray(arr)) {
+            deep(arr[i], String(key) + '[' + String(i) + ']', !Array.isArray(arr[i]))
+          } else {
+            deep(arr[i], String(key) + (isObject === undefined ? '' : '.') + String(i), !Array.isArray(arr[i]))
+          }
+        } else if (arr[i] !== null && arr[i] !== undefined) {
+          if (isObject) {
+            result[String(key) + '.' + String(i)] = arr[i]
+          } else if (isObject === false) {
+            result[String(key) + '[' + String(i) + ']'] = arr[i]
+          } else if (isObject === undefined) {
+            result[String(i)] = arr[i]
+          }
+        }
+      }
+    } else {
+      return arr
+    }
+  }
+
+  deep(arr, '')
+
+  return result
+}
+
+console.log(flatten(input))
